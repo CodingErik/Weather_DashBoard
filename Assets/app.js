dd62e74eb4945b262ed9795e51406f038ee78d41
Erik De Luna <codingerik1.gmail.com>
2020-05-24 05:48:16 -0500

    added some additional info time

--- Assets/app.js
+++ Assets/app.js
@@ -1,16 +1,27 @@
+// current day, month, day, year, time time running the seconds
+// lets try to make the clock change to the local time of the place that is searched
+$("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss A"));
+
+setInterval(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss A"));
+}, 1000);
+
+
+console.log(moment());
+
 // This is our API key. Add your own API key between the ""
 var APIKey = "9e86fc9e3bf21c56f84b81c96613709c";
 let cityName = 'Monterrey,NuevoLeon'; //input value from the search 
 
 // this is the search button
-$('searchCity').on('click', function(e){
+$('searchCity').on('click', function (e) {
     e.preventDefault();
     console.log('hello');
 });
 // Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=" + APIKey;
-                        // api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=" + APIKey;
-                        // api.openweathermap.org/data/2.5/weather?q=Monterrey,NuevoLeoni&appid=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
+// api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=" + APIKey;
+// api.openweathermap.org/data/2.5/weather?q=Monterrey,NuevoLeoni&appid=" + APIKey;
 // We then created an AJAX call
 $.ajax({
     url: queryURL,
@@ -21,45 +32,40 @@ $.ajax({
     // console.log(queryURL);
     // Create CODE HERE to log the resulting object
     console.log(response);
-    // Create CODE HERE to calculate the temperature (converted from Kelvin)
-    // let K = response.main.temp;
-    // let F = (K - 273.15) * 1.80 + 32;
-    // Create CODE HERE to transfer content to HTML
+
+    // calculating conversion from Kelvin to Farenheit
+    let K = response.list[0].main.temp;
+    let F = (K - 273.15) * 1.80 + 32;
+
+    // Current days info
+    //********************************************** */
     $(".city").text(response.city.name);
     $(".wind").text(response.list[0].wind.speed);
     $(".humidity").text(response.list[0].main.humidity);
-    // $(".temp").text(F.toFixed(1));
-    // $('.sunrise').text(moment.unix(response.sys.sunrise).format('llll'));
-    // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-    // Create CODE HERE to dump the temperature content into HTML
-
-
-    //we are gonna need for main jumbotron
+    $(".temp").text(F.toFixed(1));
+    // we still need to the UV index
+    // lon & lat for the uv index
+    // response.city.coord.lat
+    // response.city.coord.lon
     //********************************************** */
-    // city name  DONE  DISPLAYING NOW!
-    // temp         DONE DISPLAYING NOW!
-    // humidity     DONE DISPLAYING NOW!
-    // winds speed      DONE DISPLAYING NOW!
-    // UV index         DONE
-    
-    //********************************************** */
-
-
 
 
     // cards
     //********************************************** */
     // temp
     // humidity
-    // winds speed
-    // UV index  
+
 
 
     // extras 
-    // sunrise 
+    // $('.sunrise').text(moment.unix(response.sys.sunrise).format('llll'));
     //********************************************** */
-    
-    
+
+
+
+
+
+
     // search input and history
     //********************************************** */
     // event prevent default
@@ -72,12 +78,12 @@ $.ajax({
     //     // let practice sending a post request
     //     method: "GET"
     // }).then(function(response){
-    
+
     //     // we want to provide the uv index data here
     //     console.log('this is the uv index data');
     //     console.log(response);
-    
-    
+
+
     // });
 
 
