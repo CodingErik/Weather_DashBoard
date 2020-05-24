9439c7222ff86c57046d4e0f41d236943073a545
Erik De Luna <codingerik1.gmail.com>
2020-05-24 05:30:35 -0500

    finished reponsiveness started on ajax request

--- Assets/app.js
+++ Assets/app.js
@@ -1,9 +1,16 @@
 // This is our API key. Add your own API key between the ""
 var APIKey = "9e86fc9e3bf21c56f84b81c96613709c";
+let cityName = 'Monterrey,NuevoLeon'; //input value from the search 
 
+// this is the search button
+$('searchCity').on('click', function(e){
+    e.preventDefault();
+    console.log('hello');
+});
 // Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Monterrey,NuevoLeoni&appid=" + APIKey;
-
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=" + APIKey;
+                        // api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid=" + APIKey;
+                        // api.openweathermap.org/data/2.5/weather?q=Monterrey,NuevoLeoni&appid=" + APIKey;
 // We then created an AJAX call
 $.ajax({
     url: queryURL,
@@ -11,29 +18,29 @@ $.ajax({
 }).then(function (response) {
 
     // Create CODE HERE to Log the queryURL
-    console.log(queryURL);
+    // console.log(queryURL);
     // Create CODE HERE to log the resulting object
     console.log(response);
     // Create CODE HERE to calculate the temperature (converted from Kelvin)
-    let K = response.main.temp;
-    let F = (K - 273.15) * 1.80 + 32;
+    // let K = response.main.temp;
+    // let F = (K - 273.15) * 1.80 + 32;
     // Create CODE HERE to transfer content to HTML
-    $(".city").text(response.name);
-    $(".wind").text(response.wind.speed);
-    $(".humidity").text(response.main.humidity);
-    $(".temp").text(F.toFixed(1));
-    $('.sunrise').text(moment.unix(response.sys.sunrise).format('llll'));
+    $(".city").text(response.city.name);
+    $(".wind").text(response.list[0].wind.speed);
+    $(".humidity").text(response.list[0].main.humidity);
+    // $(".temp").text(F.toFixed(1));
+    // $('.sunrise').text(moment.unix(response.sys.sunrise).format('llll'));
     // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
     // Create CODE HERE to dump the temperature content into HTML
 
 
     //we are gonna need for main jumbotron
     //********************************************** */
-    // city name  DONE 
-    // temp         DONE 
-    // humidity     DONE 
-    // winds speed      DONE 
-    // UV index         
+    // city name  DONE  DISPLAYING NOW!
+    // temp         DONE DISPLAYING NOW!
+    // humidity     DONE DISPLAYING NOW!
+    // winds speed      DONE DISPLAYING NOW!
+    // UV index         DONE
     
     //********************************************** */
 
@@ -60,7 +67,23 @@ $.ajax({
     // save to local data 
     //********************************************** */
 
-
+    // $.ajax({
+    //     url: "http://api.openweathermap.org/data/2.5/uvi/forecast?lat={lat}&lon={lon}&appid="+ APIKey,
+    //     // let practice sending a post request
+    //     method: "GET"
+    // }).then(function(response){
+    
+    //     // we want to provide the uv index data here
+    //     console.log('this is the uv index data');
+    //     console.log(response);
+    
+    
+    // });
 
 
 });
+
+
+
+
+
